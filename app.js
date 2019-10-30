500fabdcdfad26078eaeafcc2252e218bbbfa778
Jarno Kinnunen <jarnoldinho@gmail.com>
2019-10-30 20:32:59 +0200

    started working with ui.js

--- app.js
+++ app.js
@@ -2,9 +2,23 @@
 
 const weather = new Weather('Helsinki', 'fi');
 
+// Get weather on Dom load
+//weather.changeLocation('kerava', 'fi');
+// document.addEventListener('DOMContentLoaded', getWeather);
+
 weather
   .getWeather()
   .then(results => {
     console.log(results);
   })
   .catch(err => console.log(err));
+
+function convertKelvinToCelsius(kelvin) {
+  if (kelvin < 0) {
+    return 'below absolute zero (0 K)';
+  } else {
+    let myCelsius = 0;
+    myCelsius = Math.round(kelvin - 273.15);
+    return myCelsius;
+  }
+}
---
+++ ui.js
@@ -0,0 +1,25 @@
+class UI {
+  constructor() {
+    this.location = document.getElementById('w-location');
+    this.desc = document.getElementById('w-desc');
+    this.string = document.getElementById('w-string');
+    this.details = document.getElementById('w-details');
+    this.icon = document.getElementById('w-icon');
+    this.humidity = document.getElementById('w-humidity');
+    //this.feelsLike = document.getElementById('w-feels-like');
+    //this.dewpoint = document.getElementById('w-dewpoint');
+    this.windSpeed = document.getElementById('w-wind-speed');
+    this.windDirection = document.getElementById('w-wind-direction');
+  }
+
+  paint(weather) {
+    this.location.textContent = `${weather.name}, ${weather.sys.country}`;
+    this.desc.textContent = weather.weather[0].main;
+    this.string.textContent = `${convertKelvinToCelsius(
+      weather.main.temp
+    )}\u00B0C`;
+    this.humidity.textContent = `Humidity: ${weather.main.humidity}%`;
+    this.windSpeed.textContent = `Wind Speed: ${weather.wind.speed} mps`;
+    this.windDirection = `Wind Direction: ${weather.wind.deg} degrees`;
+  }
+}
