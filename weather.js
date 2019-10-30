6d397407631fe9055aacfc58ffdaa0c0b7078f6c
Jarno Kinnunen <jarnoldinho@gmail.com>
2019-10-30 10:24:47 +0200

    Added weather.js

---
+++ weather.js
@@ -0,0 +1,22 @@
+class Weather {
+  constructor(city, countryCode) {
+    this.apiKey = '88aace4a39fecf769244da2a93d3aaf0';
+    this.city = city;
+    this.countryCode = countryCode;
+  }
+
+  // Fetch weather from API
+  async getWeather() {
+    const response = await fetch(
+      `api.openweathermap.org/data/2.5/weather?q=${this.city},${this.countryCode}&APPID${this.apiKey}`
+    );
+    const responseData = await response.json();
+    return responseData;
+  }
+
+  // Change wether location
+  changeLocation(city, countryCode) {
+    this.city = city;
+    this.countryCode = countryCode;
+  }
+}
